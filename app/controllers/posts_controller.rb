451c6741e73ca2f4cd20c70365bad125b0aecb22
拓美 <takumi.rug@gmail.com>
2020-07-10 16:00:28 +0900

    コントローラでフラッシュメッセージを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create!(post_params)
-    redirect_to post
+    redirect_to post, notice: "投稿しました"
   end
 
   def edit
@@ -25,13 +25,13 @@ class PostsController < ApplicationController
   def update
     # post = Post.find(params[:id])
     post.update!(post_params)
-    redirect_to post
+    redirect_to @post, notice: "更新しました"
   end
 
   def destroy
     # post = Post.find(params[:id])
     post.destroy!
-    redirect_to post
+    redirect_to @post, alert: "削除しました"
   end
 
   private
