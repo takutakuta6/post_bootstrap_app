2adfa293941313f1ddc9eda1c5a0a4c08c7b241e
拓美 <takumi.rug@gmail.com>
2020-07-12 10:23:11 +0900

    メッセージの変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create!(post_params)
-    redirect_to post, notice: "投稿しました"
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "投稿しました"
+    else
+      flash.now[:alert] = "投稿に失敗しました"
+      render :new
+    end
   end
 
   def edit
@@ -23,14 +28,17 @@ class PostsController < ApplicationController
   end
 
   def update
-    # post = Post.find(params[:id])
-    post.update!(post_params)
-    redirect_to @post, notice: "更新しました"
+    if @post.update(post_params)
+      redirect_to @post, notice: "更新しました"
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
     # post = Post.find(params[:id])
-    post.destroy!
+    @post.destroy!
     redirect_to @post, alert: "削除しました"
   end
 
