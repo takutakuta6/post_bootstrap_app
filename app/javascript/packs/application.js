20a6d71260f3f4c8dc177486433a084eaea4c26f
拓美 <takumi.rug@gmail.com>
2020-07-13 10:59:01 +0900

    差分の消し忘れのため再度コミット

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,7 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-= require jquery3
-  = require popper = require bootstrap - sprockets
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
-require("bootstrap/dist/js/bootstrap")
-require("../stylesheets/application.scss")
-
-require("bootstrap/dist/js/bootstrap")
-require("@fortawesome/fontawesome-free/js/all")
+require("bootstrap/dist/js/bootstrap");
+require("@fortawesome/fontawesome-free/js/all");
